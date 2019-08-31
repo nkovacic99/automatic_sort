3fec0a209db90aaa5630a6f8fe76669b919bd7f6
nkovacic99 <nace.kovacic007@gmail.com>
2019-08-31 13:37:23 +0200

    everything works well, automatism to do

--- automatic_sort.py
+++ automatic_sort.py
@@ -2,13 +2,16 @@ import os
 import shutil
 
 # access files in "Downloads" (check)
-# change files' locations
-# generate new folders if necessary
+# change files' locations (check)
+# generate new folders if necessary (not necessary)
+# automatic run
 
 documents = list()
 pictures = list()
 music = list()
 other = list()
+
+PATH = '/home/nace/Downloads/'
 
 koncnice = [['.txt', '.docx', '.dcx', '.pdf'], ['.jpg', '.png', '.jpeg'], ['.mp3']]
 downloads_folder = os.listdir("/home/nace/Downloads")
@@ -26,8 +29,13 @@ for datoteka in downloads_folder:
     if datoteka not in documents or datoteka not in pictures or datoteka not in music:
         other.append(datoteka)
 
-# figure out, why can't you move files
+# figure out, why can't you move files (check)
 
 for datoteka in documents:
-    shutil.move('/home/nace/Downloads', '/home/nace/Downloads/documents')
-
+    shutil.move(PATH + datoteka, PATH + 'documents/' + datoteka)
+for datoteka in pictures:
+    shutil.move(PATH + datoteka, PATH + 'pictures/' + datoteka)
+for datoteka in music:
+    shutil.move(PATH + datoteka, PATH + 'music/' + datoteka)
+for datoteka in other:
+    shutil.move(PATH + datoteka, PATH + 'other/' + datoteka)
